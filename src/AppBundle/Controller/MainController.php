851bf72ff03bbe1483ac88a4966455379d997e98
DonCallisto <samuele.lilli@gmail.com>
2016-06-05 14:23:02 +0200

    Created the route for opening boxes

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends Controller
 {
@@ -20,5 +22,19 @@ class MainController extends Controller
         return [
             'scheme' => $gameManager->getScheme(),
         ];
+    }
+
+    /**
+     * @Route("/open", name="app.main.open_box", condition="request.isXmlHttpRequest()")
+     */
+    public function openBox(Request $request)
+    {
+        $rowIndex = $request->query->get('r');
+        $columnIndex = $request->query->get('c');
+
+        $gameManager = $this->get('app.game.game_manager');
+        $result = $gameManager->open($rowIndex, $columnIndex);
+
+        return new JsonResponse(json_encode($result));
     }
 }
